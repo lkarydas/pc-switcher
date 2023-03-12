675546000374841b97fbd8379b6ef5e071c70d99
lkarydas <lkarydas@gmail.com>
2023-03-12 17:05:25 -0400

    Cleanup.

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ except RuntimeError:
           'You can achieve this by using "sudo" to run your script')
 
 FLAGS = flags.FLAGS
-flags.DEFINE_integer("num_times", 1,
-                     "Number of times to print greeting.")
+# No flags for now.
 
 # Set up GPIO pins.
 GPIO.setmode(GPIO.BOARD)
@@ -24,34 +23,28 @@ GPIO.setup(LED_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(LED_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(LED_3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(LED_4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 def main(argv):
   del argv  # Unused.
   led_values = [0, 0, 0, 0]
   try:  
-    # here you put your main loop or block of code  
     while True:  
       new_led_values = ([GPIO.input(LED_1), GPIO.input(LED_2), GPIO.input(LED_3), GPIO.input(LED_4)])
       if sum(new_led_values) != 1:
-        continue  # Transition between LEDs.
+        continue  # Discard transitions between LEDs.
       if new_led_values != led_values:
         led_values = new_led_values
         print(led_values.index(1) + 1)
     time.sleep(0.3)
   
   except KeyboardInterrupt:  
-    # here you put any code you want to run before the program   
-    # exits when you press CTRL+C  
-    print('\nBye!') # print value of counter  
+    print('\nBye!')
   
   except Exception as e:  
-    # this catches ALL other exceptions including errors.  
-    # You won't get any error messages for debugging  
-    # so only use it once your code is working  
     print(str(e))
   
   finally:  
-    GPIO.cleanup() # this ensures a clean exit  
-
+    GPIO.cleanup()
 
 if __name__ == '__main__':
   app.run(main)
