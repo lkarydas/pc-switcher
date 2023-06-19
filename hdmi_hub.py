80d5251d3408314f35550dec87854e31c5d333c4
Lazaros Karydas <lkarydas@users.noreply.github.com>
2023-06-19 14:43:54 -0400

    Fix lint errors.

--- hdmi_hub.py
+++ hdmi_hub.py
@@ -1,5 +1,7 @@
-""" Controls the HDMI switch using ir-ctl.  """
-# Assumes that lirc is already set up.
+""" Controls the HDMI switch using ir-ctl.
+
+Assumes that lirc is already set up.
+"""
 
 import subprocess
 
@@ -13,8 +15,9 @@ IR_COMMAND_MAP = {
 }
 
 def switch_to(hdmi_position):
+  """Send IR command to HDMI switch to change input."""
   ir_command = IR_COMMAND_MAP[hdmi_position]
   logging.info('Switching HDMI hub to position %s.', hdmi_position)
   command = ['ir-ctl', '-S', ir_command]
   logging.info('Running command: %s', ' '.join(command))
-  subprocess.run(command)
+  subprocess.run(command, check=False)
