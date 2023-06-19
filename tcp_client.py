33b18a535317632711fd8fe675eb377eabc9c283
Lazaros Karydas <lkarydas@users.noreply.github.com>
2023-06-19 14:36:10 -0400

    Fix lint errors.

--- tcp_client.py
+++ tcp_client.py
@@ -1,9 +1,12 @@
+"""TCP client for sending messages."""
+
 import socket
 
 HOST = "192.168.1.153"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 def send_message(message):
-  clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  clientSocket.connect((HOST, PORT))
-  clientSocket.send(message.encode())
+  """Send a TCP message."""
+  client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client_socket.connect((HOST, PORT))
+  client_socket.send(message.encode())
