9580f82596673ebd6ebb16dbdf5223d8bbf77b59
Lazaros Karydas <lkarydas@users.noreply.github.com>
2023-12-25 21:49:33 -0500

    Fix formatting.

--- switch_daemon_main.py
+++ switch_daemon_main.py
@@ -26,13 +26,13 @@ FLAGS = flags.FLAGS
 
 @dataclasses.dataclass
 class PanelButton:
-  """Data class that represents a panel button."""
-  pin: int
-  led_pin: int
-  usb_position: int
-  computer_name: str
-  button_color: str
-  hdmi_position: int
+    """Data class that represents a panel button."""
+    pin: int
+    led_pin: int
+    usb_position: int
+    computer_name: str
+    button_color: str
+    hdmi_position: int
 
 
 _PANEL_BUTTON_MAP = {
@@ -48,109 +48,111 @@ MAX_ATTEMPTS = 3
 
 
 class PanelController:
-  """Controls what happens when panel buttons are pressed."""
+    """Controls what happens when panel buttons are pressed."""
 
-  def __init__(self):
-    self.hub_led_input_devices = list([gpiozero.InputDevice(pin) for pin in
-                                       USB_HUB_LED_PINS])
-    self.hub_button = gpiozero.OutputDevice(USB_HUB_BUTTON_PIN)
-    self.button_leds = {
-        13: gpiozero.LED(21),
-        5: gpiozero.LED(20),
-        6: gpiozero.LED(16),
-        19: gpiozero.LED(12, active_high=False),
-    }
+    def __init__(self):
+        self.hub_led_input_devices = list([gpiozero.InputDevice(pin) for pin in
+                                           USB_HUB_LED_PINS])
+        self.hub_button = gpiozero.OutputDevice(USB_HUB_BUTTON_PIN)
+        self.button_leds = {
+            5: gpiozero.LED(20),
+            6: gpiozero.LED(16),
+            13: gpiozero.LED(21),
+            19: gpiozero.LED(12, active_high=False),
+        }
 
-  def button_callback(self, button):
-    """Callback fn for panel button press."""
-    panel_button = _PANEL_BUTTON_MAP[button.pin.number]
-    print('')
-    logging.info(
-        'Button pressed! Pin: %s Switching to %s.',
-        panel_button.pin,
-        panel_button.computer_name)
-    self.turn_off_all_button_leds()
-    self.button_leds[button.pin.number].on()
-    logging.info(f'Switching USB hub to position {panel_button.usb_position}.')
-    self.switch_to(panel_button.usb_position)
-    hdmi_position = panel_button.hdmi_position
-    logging.info('hdmi_position: %i', hdmi_position)
-    hdmi_hub.switch_to(2)  # So that C730 can send the DDC command.
-    time.sleep(2)  # To give time for the HDMI swicth.
-    if panel_button.computer_name == 'MSI':
-      logging.info('Sending UDP command to switch monitor input to DP2.')
-      tcp_client.send_message('DP2')
-    else:
-      logging.info('Sending UDP command to switch monitor input to HDMI.')
-      tcp_client.send_message('HDMI')
-    hdmi_hub.switch_to(hdmi_position)
+    def button_callback(self, button):
+        """Callback fn for panel button press."""
+        panel_button = _PANEL_BUTTON_MAP[button.pin.number]
+        print('')
+        logging.info(
+            'Button pressed! Pin: %s Switching to %s.',
+            panel_button.pin,
+            panel_button.computer_name)
+        self.turn_off_all_button_leds()
+        self.button_leds[button.pin.number].on()
+        logging.info(
+            f'Switching USB hub to position {panel_button.usb_position}.')
+        self.switch_to(panel_button.usb_position)
+        hdmi_position = panel_button.hdmi_position
+        logging.info('hdmi_position: %i', hdmi_position)
+        hdmi_hub.switch_to(2)  # So that C730 can send the DDC command.
+        time.sleep(2)  # To give time for the HDMI swicth.
+        if panel_button.computer_name == 'MSI':
+            logging.info('Sending UDP command to switch monitor input to DP2.')
+            tcp_client.send_message('DP2')
+        else:
+            logging.info(
+                'Sending UDP command to switch monitor input to HDMI.')
+            tcp_client.send_message('HDMI')
+        hdmi_hub.switch_to(hdmi_position)
 
-  def turn_off_all_button_leds(self):
-    """Turn off all LEDS on the panel buttons."""
-    for _, led in self.button_leds.items():
-      led.off()
+    def turn_off_all_button_leds(self):
+        """Turn off all LEDS on the panel buttons."""
+        for _, led in self.button_leds.items():
+            led.off()
 
-  def register_button_callbacks(self):
-    """Register callbacks for panel buttons."""
-    button_1 = gpiozero.Button(5)
-    button_2 = gpiozero.Button(6)
-    button_3 = gpiozero.Button(13)
-    button_4 = gpiozero.Button(19)
-    button_1.when_pressed = self.button_callback
-    button_2.when_pressed = self.button_callback
-    button_3.when_pressed = self.button_callback
-    button_4.when_pressed = self.button_callback
+    def register_button_callbacks(self):
+        """Register callbacks for panel buttons."""
+        button_1 = gpiozero.Button(5)
+        button_2 = gpiozero.Button(6)
+        button_3 = gpiozero.Button(13)
+        button_4 = gpiozero.Button(19)
+        button_1.when_pressed = self.button_callback
+        button_2.when_pressed = self.button_callback
+        button_3.when_pressed = self.button_callback
+        button_4.when_pressed = self.button_callback
 
-  def press_hub_button(self):
-    """Simulate a button press to the USB switch (advances to next input)."""
-    self.hub_button.on()
-    time.sleep(0.1)
-    self.hub_button.off()
-
-  def switch_to(self, position):
-    """Switch USB switch to a specific position."""
-    current_position = self.get_current_position()
-    while current_position != position:
-      self.press_hub_button()
-      time.sleep(0.1)
-      current_position = self.get_current_position()
-
-  def get_current_position(self):
-    """Returns the current position of the USB switch."""
-    led_values = self.read_led_values()
-    if not led_values:
-      logging.error('Error - led values is: %s', led_values)
-      return 0
-    current_position = led_values.index(1) + 1
-    return current_position
-
-  def read_led_values(self):
-    """Read the LED values from USB switch and return them."""
-    for _ in range(0, MAX_ATTEMPTS):
-      led_values = list(
-          [device.value for device in self.hub_led_input_devices])
-      if sum(led_values) != 1:
+    def press_hub_button(self):
+        """Send a button press to the USB switch (advances to next input)."""
+        self.hub_button.on()
         time.sleep(0.1)
-        continue  # Discard transitions between LEDs.
-      return led_values
+        self.hub_button.off()
+
+    def switch_to(self, position):
+        """Switch USB switch to a specific position."""
+        current_position = self.get_current_position()
+        while current_position != position:
+            self.press_hub_button()
+            time.sleep(0.1)
+            current_position = self.get_current_position()
+
+    def get_current_position(self):
+        """Returns the current position of the USB switch."""
+        led_values = self.read_led_values()
+        if not led_values:
+            logging.error('Error - led values is: %s', led_values)
+            return 0
+        current_position = led_values.index(1) + 1
+        return current_position
+
+    def read_led_values(self):
+        """Read the LED values from USB switch and return them."""
+        for _ in range(0, MAX_ATTEMPTS):
+            led_values = list(
+                [device.value for device in self.hub_led_input_devices])
+            if sum(led_values) != 1:
+                time.sleep(0.1)
+                continue  # Discard transitions between LEDs.
+            return led_values
 
 
 def main(argv):
-  """Main function."""
-  del argv  # Unused.
-  logging.set_verbosity(logging.INFO)
-  logging.get_absl_handler().use_absl_log_file()
-  logging.info('Main loop.')
-  controller = PanelController()
-  controller.register_button_callbacks()
-  # Signal pause only works on Linux.
-  signal.pause()  # pylint: disable=no-member
+    """Main function."""
+    del argv  # Unused.
+    logging.set_verbosity(logging.INFO)
+    logging.get_absl_handler().use_absl_log_file()
+    logging.info('Main loop.')
+    controller = PanelController()
+    controller.register_button_callbacks()
+    # Signal pause only works on Linux.
+    signal.pause()  # pylint: disable=no-member
 
 
 if __name__ == '__main__':
-  try:
-    app.run(main)
-  except KeyboardInterrupt:
-    print('')
-    logging.info('Bye!')
-    sys.exit(0)
+    try:
+        app.run(main)
+    except KeyboardInterrupt:
+        print('')
+        logging.info('Bye!')
+        sys.exit(0)
