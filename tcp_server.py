cf7f41ac38fad0cc62d481a7c916219fafae24f6
Lazaros Karydas <lkarydas@users.noreply.github.com>
2023-12-25 22:10:08 -0500

    Formatting.

--- tcp_server.py
+++ tcp_server.py
@@ -11,16 +11,17 @@ HOST = ""  # Standard loopback interface address (localhost).
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023).
 TCPServer = socketserver.TCPServer
 
+
 def main(argv):
-  """Main function that starts the TCP server for DDC monitor control."""
-  del argv  # Unused.
-  logging.info('Server is accepting connections on port %s.', PORT)
-  with TCPServer((HOST, PORT), ddc_control_tcp_handler.DDCHandler) as server:
-    try:
-      server.serve_forever()
-    except KeyboardInterrupt:
-      logging.info('Exiting.')
+    """Main function that starts the TCP server for DDC monitor control."""
+    del argv  # Unused.
+    logging.info('Server is accepting connections on port %s.', PORT)
+    with TCPServer((HOST, PORT), ddc_control_tcp_handler.DDCHandler) as server:
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            logging.info('Exiting.')
 
 
 if __name__ == '__main__':
-  app.run(main)
+    app.run(main)
