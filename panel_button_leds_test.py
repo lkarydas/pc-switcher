4da9d69593287d41304de45d87117c6a2a43e8bc
Lazaros Karydas <lkarydas@users.noreply.github.com>
2024-01-16 19:28:18 -0500

    Fix led test.

--- panel_button_leds_test.py
+++ panel_button_leds_test.py
@@ -5,13 +5,6 @@ import pi_header_pinout
 import panel_button_leds
 
 
-BUTTON_PINS = [
-    pi_header_pinout.BUTTON_1_GPIO_PIN,
-    pi_header_pinout.BUTTON_2_GPIO_PIN,
-    pi_header_pinout.BUTTON_3_GPIO_PIN,
-    pi_header_pinout.BUTTON_4_GPIO_PIN,
-]
-
 controller = panel_button_leds.PanelButtonLEDsController()
 
 print('This will flash all LEDs once, then turn them on and off sequentially.')
@@ -24,8 +17,8 @@ time.sleep(0.5)
 
 for i in range(4):
     print(f'Turning on LED {i + 1}.')
-    controller.turn_on_led(BUTTON_PINS[i])
+    controller.turn_on_led(pi_header_pinout.BUTTON_LED_GPIO_PINS[i])
     time.sleep(0.6)
-    controller.turn_off_led(BUTTON_PINS[i])
+    controller.turn_off_led(pi_header_pinout.BUTTON_LED_GPIO_PINS[i])
     time.sleep(0.1)
 print('Done.')
